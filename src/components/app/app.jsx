0b57ed929ee89fe3f40e88904ea1ee81400043e0
Андрей Филиппов <filippoussis@iMac-AndrosPhilippos.local>
2020-10-11 20:47:50 +0500

    Рефакт компонентов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,32 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
-import MainPage from "components/main-page/main-page";
-import SignInScreen from "components/sign-in-screen/sign-in-screen";
-import MyListScreen from "components/my-list-screen/my-list-screen";
-import PlayerScreen from "components/player-screen/player-screen";
-import MoviePage from "components/movie-page/movie-page";
-import MovieAddReview from "components/movie-add-review/movie-add-review";
+import MainPage from "@components/main-page/main-page";
+import SignInScreen from "@components/sign-in-screen/sign-in-screen";
+import MyListScreen from "@components/my-list-screen/my-list-screen";
+import PlayerScreen from "@components/player-screen/player-screen";
+import MoviePage from "@components/movie-page/movie-page";
+import MovieAddReview from "@components/movie-add-review/movie-add-review";
 
 const App = (props) => {
 
-  const {title, genre, release} = props;
+  const {title, genre, release, films} = props;
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <MainPage title = {title} genre = {genre} release = {release}/>
-        </Route>
+
+        <Route
+          exact path="/"
+          render={({history}) => (
+            <MainPage
+              onAvatarClick={() => history.push(`/mylist`)}
+              onPlayButtonClick={() => history.push(`/player/:id`)}
+              onCardClick={() => history.push(`/films/:id`)}
+              title={title}
+              genre={genre}
+              release={release}
+              films={films}
+            />
+          )}
+        />
+
         <Route exact path="/login">
           <SignInScreen />
         </Route>
         <Route exact path="/mylist">
-          <MyListScreen />
+          <MyListScreen films={films} />
         </Route>
-        <Route exact component={PlayerScreen} path="/player/:id?"></Route>
-        <Route exact component={MovieAddReview} path="/films/review"></Route>
-        <Route exact component={MoviePage} path="/films/:id?"></Route>
+        <Route
+          exact path="/player/:id"
+          render={({history}) => (
+            <PlayerScreen onExitButtonClick={() => history.push(`/`)} />
+          )}
+        />
+
+        <Route exact path="/films/:id/review">
+          <MovieAddReview />
+        </Route>
+
+        <Route exact path="/films/:id">
+          <MoviePage />
+        </Route>
+
       </Switch>
     </BrowserRouter>
   );
@@ -36,6 +61,7 @@ App.propTypes = {
   title: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   release: PropTypes.string.isRequired,
+  films: PropTypes.array.isRequired,
 };
 
 export default App;
--- src/components/player-screen/player-screen.jsx
+++ src/components/player-screen/player-screen.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-const PlayerScreen = () => {
+const PlayerScreen = (props) => {
+
+  const {onExitButtonClick} = props;
 
   return (
     <div className="player">
       <video src="#" className="player__video" poster="img/player-poster.jpg"></video>
 
-      <button type="button" className="player__exit">Exit</button>
+      <button type="button" className="player__exit" onClick={onExitButtonClick}>Exit</button>
 
       <div className="player__controls">
         <div className="player__controls-row">
@@ -36,6 +39,10 @@ const PlayerScreen = () => {
       </div>
     </div>
   );
+};
+
+PlayerScreen.propTypes = {
+  onExitButtonClick: PropTypes.func.isRequired,
 };
 
 export default PlayerScreen;
