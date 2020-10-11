ea4896763ecce48b3a83658177feec813989f50a
Андрей Филиппов <filippoussis@iMac-AndrosPhilippos.local>
2020-10-11 20:42:12 +0500

    Рефакт index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "components/app/app";
+import App from "@components/app/app";
+import films from "./mocks/films";
 
 const Annotation = {
-  TITLE: `Blade Runner 2049`,
-  GENRE: `Fantastic`,
-  RELEASE: `2017`
+  TITLE: `Sully`,
+  GENRE: `Drama`,
+  RELEASE: `2016`
 };
 
 ReactDOM.render(
-    <App title={Annotation.TITLE} genre={Annotation.GENRE} release={Annotation.RELEASE} />,
+    <App
+      title={Annotation.TITLE}
+      genre={Annotation.GENRE}
+      release={Annotation.RELEASE}
+      films={films}
+    />,
     document.querySelector(`#root`)
 );
