fd7af0be9ef4d26c7ad8cb587e71873aaac8629a
Андрей Филиппов <filippoussis@iMac-AndrosPhilippos.local>
2020-11-11 02:03:21 +0500

    Расширил моковые данные

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,7 +2,7 @@ export default [
   {
     id: `1`,
     title: `Blade Runner 2049`,
-    genre: [`Sci-Fi`, `Fantasy`, `Suspense`, `Thriller`],
+    genre: `Sci-Fi`,
     release: `2017`,
     about: `Thirty years after the events of the first film, a new blade runner, LAPD Officer K (Ryan Gosling),
             unearths a long buried secret that has the potential to plunge what’s left of society into chaos.
@@ -17,7 +17,7 @@ export default [
   }, {
     id: `2`,
     title: `Mad Max: Fury Road`,
-    genre: [`Sci-Fi`, `Fantasy`, `Action`, `Adventure`],
+    genre: `Horror`,
     release: `2015`,
     about: `From filmmaker George Miller, originator of the post-apocalyptic genre and mastermind behind the legendary Mad Max trilogy,
             comes this return to the world of the Road Warrior, Max Rockatansky. Haunted by his turbulent past,
@@ -34,7 +34,7 @@ export default [
   }, {
     id: `3`,
     title: `Dunkirk`,
-    genre: [`Action`, `Drama`, `History`],
+    genre: `Dramas`,
     release: `2017`,
     about: `"Dunkirk" opens as hundreds of thousands of British and Allied troops are surrounded by enemy forces.
             Trapped on the beach with their backs to the sea they face an impossible situation as the enemy closes in.`,
@@ -47,7 +47,7 @@ export default [
   }, {
     id: `4`,
     title: `Ready Player One`,
-    genre: [`Sci-Fi`, `Fantasy`, `Action`, `Adventure`],
+    genre: `Sci-Fi`,
     release: `2018`,
     about: `From filmmaker Steven Spielberg comes the science fiction action adventure “Ready Player One”,
             based on Ernest Cline’s bestseller of the same name. The film is set in 2045,
@@ -65,7 +65,7 @@ export default [
   }, {
     id: `5`,
     title: `A Star Is Born`,
-    genre: [`Drama`, `Music`, `Romance`],
+    genre: `Romance`,
     release: `2018`,
     about: `Seasoned musician Jackson Maine (Bradley Cooper) discovers—and falls in love with—struggling artist Ally (Lady Gaga).
             She has just about given up on her dream to make it big as a singer… until Jack coaxes her into the spotlight.
@@ -80,7 +80,7 @@ export default [
   }, {
     id: `6`,
     title: `Smallfoot`,
-    genre: [`Animation`, `Comedy`, `Family`],
+    genre: `Kids & Family`,
     release: `2018`,
     about: `An animated adventure for all ages, with original music and an all-star cast,
             "Smallfoot" turns the Bigfoot legend upside down when a bright young Yeti (Channing Tatum) finds something he thought didn’t exist—a human.
@@ -96,7 +96,7 @@ export default [
   }, {
     id: `7`,
     title: `King Arthur: Legend of the Sword`,
-    genre: [`Action`, `Adventure`, `Drama`],
+    genre: `Dramas`,
     release: `2018`,
     about: `Acclaimed filmmaker Guy Ritchie brings his dynamic style to the epic fantasy action adventure King Arthur:
             Legend of the Sword. Starring Charlie Hunnam in the title role, the film is an iconoclastic take on the classic Excalibur myth,
@@ -113,7 +113,135 @@ export default [
   }, {
     id: `8`,
     title: `Tomb Raider`,
-    genre: [`Action`, `Adventure`, `Fantasy`],
+    genre: `Dramas`,
+    release: `2018`,
+    about: `Lara Croft (Alicia Vikander) is the independent daughter of an eccentric adventurer who mysteriously vanished when she was a teen.
+            Determined to forge her own path, she refuses to take the reins of her father’s global empire just as she rejects the idea that he’s truly gone.
+            Against her father’s final wishes, Lara leaves everything she knows behind in search of her dad’s last-known destination: a fabled tomb on a mythical island.
+            Her mission will not be easy; just reaching the island will be extremely treacherous. Against all odds and armed with only her sharp mind,
+            blind faith and inherently stubborn spirit, she must learn to push herself beyond her limits as she journeys into the unknown.
+            If she survives this perilous adventure, she could earn the name Tomb Raider.`,
+    director: `Roar Uthaug`,
+    stars: [`Alicia Vikander`, `Dominic West`, `Walton Goggins`],
+    time: `1h 59min`,
+    poster: `tomb-raider-poster`,
+    preview: `tomb-raider-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `9`,
+    title: `Blade Runner 2049`,
+    genre: `Sci-Fi`,
+    release: `2017`,
+    about: `Thirty years after the events of the first film, a new blade runner, LAPD Officer K (Ryan Gosling),
+            unearths a long buried secret that has the potential to plunge what’s left of society into chaos.
+            K’s discovery leads him on a quest to find Rick Deckard (Harrison Ford),
+            a former LAPD blade runner who has been missing for 30 years.`,
+    director: `Denis Villeneuve`,
+    stars: [`Ryan Gosling`, `Dave Bautista`, `Robin Wright`],
+    time: `2h 44min`,
+    poster: `bladerunner2049-poster`,
+    preview: `bladerunner2049-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `10`,
+    title: `Mad Max: Fury Road`,
+    genre: `Horror`,
+    release: `2015`,
+    about: `From filmmaker George Miller, originator of the post-apocalyptic genre and mastermind behind the legendary Mad Max trilogy,
+            comes this return to the world of the Road Warrior, Max Rockatansky. Haunted by his turbulent past,
+            Mad Max (Tom Hardy - The Dark Knight Rises) believes the best way to survive is to wander alone.
+            Nevertheless, he becomes swept up with a band of survivors fleeing across the Wasteland in a war rig driven by an elite Imperator,
+            Furiosa (Oscar ® winner Charlize Theron - Prometheus). They are escaping a Citadel tyrannized by the Immortan Joe (Hugh Keays-Byrne),
+            from whom something irreplaceable has been taken. Enraged, the Warlord marshals all his gangs and ruthlessly pursues the rebels in the high-octane Road War that follows.`,
+    director: `George Miller`,
+    stars: [`Tom Hardy`, `Charlize Theron`, `Nicholas Hoult`],
+    time: `2h`,
+    poster: `madmax-poster`,
+    preview: `madmax-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `11`,
+    title: `Dunkirk`,
+    genre: `Dramas`,
+    release: `2017`,
+    about: `"Dunkirk" opens as hundreds of thousands of British and Allied troops are surrounded by enemy forces.
+            Trapped on the beach with their backs to the sea they face an impossible situation as the enemy closes in.`,
+    director: `Christopher Nolan`,
+    stars: [`Fionn Whitehead`, `Barry Keoghan`, `Mark Rylance`],
+    time: `1h 46min`,
+    poster: `dunkirk-poster`,
+    preview: `dunkirk-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `12`,
+    title: `Ready Player One`,
+    genre: `Sci-Fi`,
+    release: `2018`,
+    about: `From filmmaker Steven Spielberg comes the science fiction action adventure “Ready Player One”,
+            based on Ernest Cline’s bestseller of the same name. The film is set in 2045,
+            with the world on the brink of chaos and collapse. But the people have found salvation in the OASIS,
+            an expansive virtual reality universe created by the brilliant and eccentric James Halliday (Mark Rylance).
+            When Halliday dies, he leaves his immense fortune to the first person to find a digital Easter egg he has hidden somewhere in the OASIS,
+            sparking a contest that grips the entire world. When an unlikely young hero named Wade Watts (Tye Sheridan) decides to join the contest,
+            he is hurled into a breakneck, reality-bending treasure hunt through a fantastical universe of mystery, discovery and danger.`,
+    director: `Steven Spielberg`,
+    stars: [`Tye Sheridan`, `Olivia Cooke`, `Ben Mendelsohn`],
+    time: `2h 20min`,
+    poster: `ready-player-one-poster`,
+    preview: `ready-player-one-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `13`,
+    title: `A Star Is Born`,
+    genre: `Romance`,
+    release: `2018`,
+    about: `Seasoned musician Jackson Maine (Bradley Cooper) discovers—and falls in love with—struggling artist Ally (Lady Gaga).
+            She has just about given up on her dream to make it big as a singer… until Jack coaxes her into the spotlight.
+            But even as Ally’s career takes off, the personal side of their relationship is breaking down,
+            as Jack fights an ongoing battle with his own internal demons.`,
+    director: `Bradley Cooper`,
+    stars: [`Lady Gaga`, `Bradley Cooper`, `Sam Elliott`],
+    time: `2h 16min`,
+    poster: `a-star-is-born-poster`,
+    preview: `a-star-is-born-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `14`,
+    title: `Smallfoot`,
+    genre: `Kids & Family`,
+    release: `2018`,
+    about: `An animated adventure for all ages, with original music and an all-star cast,
+            "Smallfoot" turns the Bigfoot legend upside down when a bright young Yeti (Channing Tatum) finds something he thought didn’t exist—a human.
+            News of this “smallfoot” (James Corden) brings him fame and a chance with the girl of his dreams.
+            It also throws the simple Yeti community into an uproar over what else might be out there in the big world beyond their snowy village,
+            in a rollicking story about friendship, courage and the joy of discovery.`,
+    director: `Karey Kirkpatrick`,
+    stars: [`Channing Tatum`, `James Corden`, `Zendaya`],
+    time: `1h 36min`,
+    poster: `smallfoot-poster`,
+    preview: `smallfoot-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `15`,
+    title: `King Arthur: Legend of the Sword`,
+    genre: `Dramas`,
+    release: `2018`,
+    about: `Acclaimed filmmaker Guy Ritchie brings his dynamic style to the epic fantasy action adventure King Arthur:
+            Legend of the Sword. Starring Charlie Hunnam in the title role, the film is an iconoclastic take on the classic Excalibur myth,
+            tracing Arthur’s journey from the streets to the throne.
+            When the child Arthur’s father is murdered, Vortigern (Jude Law), Arthur’s uncle, seizes the crown.
+            Robbed of his birthright and with no idea who he truly is, Arthur comes up the hard way in the back alleys of the city.
+            But once he pulls the sword from the stone, his life is turned upside down and he is forced to acknowledge his true legacy... whether he likes it or not.`,
+    director: `Guy Ritchie`,
+    stars: [`Charlie Hunnam`, `Astrid Bergès-Frisbey`, `Jude Law`],
+    time: `2h 6min`,
+    poster: `king-arthur-poster`,
+    preview: `king-arthur-preview`,
+    trailer: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }, {
+    id: `16`,
+    title: `Tomb Raider`,
+    genre: `Dramas`,
     release: `2018`,
     about: `Lara Croft (Alicia Vikander) is the independent daughter of an eccentric adventurer who mysteriously vanished when she was a teen.
             Determined to forge her own path, she refuses to take the reins of her father’s global empire just as she rejects the idea that he’s truly gone.
