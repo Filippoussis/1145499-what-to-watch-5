e2aea8b5059416ee2c10d3c8a73dd3c1c5ff94b2
Андрей Филиппов <filippoussis@iMac-AndrosPhilippos.local>
2020-11-11 02:01:54 +0500

    Создал компоненты для блока навигации

---
+++ src/components/movie-details/movie-details.jsx
@@ -0,0 +1,44 @@
+import filmProp from '../../props/film';
+
+const MovieDetails = (props) => {
+
+  const {director, stars, time, genre, release} = props.film;
+
+  return (
+    <div className="movie-card__text movie-card__row">
+      <div className="movie-card__text-col">
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Director</strong>
+          <span className="movie-card__details-value">{director}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Starring</strong>
+          <span className="movie-card__details-value">
+            {stars.join(`, `)}
+          </span>
+        </p>
+      </div>
+
+      <div className="movie-card__text-col">
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Run Time</strong>
+          <span className="movie-card__details-value">{time}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Genre</strong>
+          <span className="movie-card__details-value">{genre.join(`, `)}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Released</strong>
+          <span className="movie-card__details-value">{release}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+MovieDetails.propTypes = {
+  film: filmProp,
+};
+
+export default MovieDetails;
---
+++ src/components/movie-reviews/movie-reviews.jsx
@@ -0,0 +1,91 @@
+const MovieReviews = () => {
+
+  const text = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit voluptates iste tempore aliquid cum ipsa. Nemo corrupti, quibusdam magnam unde doloribus dolores rem delectus, minus culpa labore quis quo soluta!`;
+
+  return (
+    <div className="movie-card__reviews movie-card__row">
+      <div className="movie-card__reviews-col">
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">{text}</p>
+
+            <footer className="review__details">
+              <cite className="review__author">Kate Muir</cite>
+              <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
+            </footer>
+          </blockquote>
+
+          <div className="review__rating">8,9</div>
+        </div>
+
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">{text}</p>
+
+            <footer className="review__details">
+              <cite className="review__author">Bill Goodykoontz</cite>
+              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+            </footer>
+          </blockquote>
+
+          <div className="review__rating">8,0</div>
+        </div>
+
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">{text}</p>
+
+            <footer className="review__details">
+              <cite className="review__author">Amanda Greever</cite>
+              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+            </footer>
+          </blockquote>
+
+          <div className="review__rating">8,0</div>
+        </div>
+      </div>
+      <div className="movie-card__reviews-col">
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">{text}</p>
+
+            <footer className="review__details">
+              <cite className="review__author">Matthew Lickona</cite>
+              <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+            </footer>
+          </blockquote>
+
+          <div className="review__rating">7,2</div>
+        </div>
+
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">{text}</p>
+
+            <footer className="review__details">
+              <cite className="review__author">Paula Fleri-Soler</cite>
+              <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+            </footer>
+          </blockquote>
+
+          <div className="review__rating">7,6</div>
+        </div>
+
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">{text}</p>
+
+            <footer className="review__details">
+              <cite className="review__author">Paula Fleri-Soler</cite>
+              <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+            </footer>
+          </blockquote>
+
+          <div className="review__rating">7,0</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieReviews;
