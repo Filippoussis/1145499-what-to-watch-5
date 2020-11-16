a3821c34ebd951b8e0842f9b5af5b7c046849f63
Андрей Филиппов <filippoussis@iMacAndhilippos.planet-a.ru>
2020-11-16 23:37:43 +0500

    Создал компоненент Filter

---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,40 @@
+const GENRES = [
+  `All genres`,
+  `Comedies`,
+  `Crime`,
+  `Documentary`,
+  `Dramas`,
+  `Horror`,
+  `Kids & Family`,
+  `Romance`,
+  `Sci-Fi`,
+  `Thrillers`
+]
+
+const Filter = (props) => {
+
+  const {selectedFilter, onFilterClick} = props;
+
+  const filterItems = GENRES.map((item) => {
+
+    const isActive = selectedFilter === item;
+    const classMod = isActive ? `catalog__genres-item--active` : ``;
+
+    return (
+      <li
+        key={item}
+        className={`catalog__genres-item ${classMod}`}
+        onClick={() => onFilterClick(item)} >
+        <span className="catalog__genres-link">{item}</span>
+      </li>
+    );
+  });
+
+  return (
+    <ul className="catalog__genres-list">
+      {filterItems}
+    </ul>
+  )
+};
+
+export default Filter;
